faf5ddf7b01af13a476eacdb6d518b85fc7cbb1d
FatenRostom-at-733083339469 <faten@gmail.com>
2024-11-19 18:38:11 +0100

    task2

--- jmp-cloud-bank-impl/src/main/java/com/epam/jmp/impl/BankImpl.java
+++ jmp-cloud-bank-impl/src/main/java/com/epam/jmp/impl/BankImpl.java
@@ -1,13 +1,23 @@
 package com.epam.jmp.impl;
 
-import com.epam.jmp.dto.BankCard;
-import com.epam.jmp.dto.BankCardType;
-import com.epam.jmp.dto.User;
+import com.epam.jmp.dto.*;
 import com.epam.jmp.service.Bank;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.function.BiFunction;
+
 public class BankImpl implements Bank {
+    Map<BankCardType, BiFunction<User, Double, BankCard>> cardCreator = new HashMap<>();
+
+    public BankImpl() {
+        cardCreator.put(BankCardType.CREDIT, (user, limit) -> new CreditBankCard(UUID.randomUUID().toString(), user, limit));
+        cardCreator.put(BankCardType.DEBIT, (user, balance) -> new DebitBankCard(UUID.randomUUID().toString(), user, balance));
+    }
+
     @Override
     public BankCard createBankCard(User user, BankCardType cardType) {
-        return null;
+        return cardCreator.get(cardType).apply(user, 0d);
     }
 }
